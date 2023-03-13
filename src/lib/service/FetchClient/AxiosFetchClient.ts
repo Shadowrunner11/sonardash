21ba83030230fb7771ea9d554ade449a52e12376
shadowrunner11 <pieroramirez810@gmail.com>
2023-03-13 08:20:30 -0500

    :sparkles: added axios fetch client cache

--- src/lib/service/FetchClient/AxiosFetchClient.ts
+++ src/lib/service/FetchClient/AxiosFetchClient.ts
@@ -1,14 +1,28 @@
 import Axios, { AxiosInstance } from 'axios'
+import { setupCache, buildMemoryStorage } from 'axios-cache-interceptor/dev'
 import { FetchClientOptions, IFetchClient, IHeaders, ILogger, PojoType } from '../../../types'
+import { exposeToGlobal } from '../../../utils'
+import { FetchClientWithHelpers } from './FetchClientWithHelpers'
 
-export class AxiosFetchClient implements IFetchClient {
+export class AxiosFetchClient extends FetchClientWithHelpers implements IFetchClient {
   private client: AxiosInstance
-  constructor({ baseURL, headers, auth }: FetchClientOptions) {
-    this.client = Axios.create({
-      baseURL,
-      headers,
-      auth,
-    })
+  constructor(options: FetchClientOptions) {
+    const { baseURL, headers, auth } = options
+
+    super(options)
+
+    const storage = buildMemoryStorage()
+    exposeToGlobal(storage, 'axiosStorage')
+
+    this.client = setupCache(
+      Axios.create({
+        baseURL,
+        headers,
+        auth,
+      }),
+      // eslint-disable-next-line no-console
+      { debug: console.log, storage, interpretHeader: false }
+    )
   }
 
   async get<T = unknown, K = PojoType>(url: string, params?: K, headers?: IHeaders): Promise<T> {
