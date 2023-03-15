c6a4452aa5644dcfa5996a4144173d8d41f98445
shadowrunner11 <pieroramirez810@gmail.com>
2023-03-15 03:42:20 -0500

    :test_tube: browse router rewrite

--- src/App.tsx
+++ src/App.tsx
@@ -2,30 +2,33 @@ import { Resource } from 'react-admin'
 import AdminSonarQube from './layout/AdminSonarQube'
 import { lazy } from 'react'
 import { DefaultLazy } from './components/DefaultLazy'
+import { BrowserRouter } from 'react-router-dom'
 
 const LazyIssueList = lazy(() => import('./components/IssueList'))
 const LazyProjectList = lazy(() => import('./components/ProjectsList'))
 
 function App() {
   return (
-    <AdminSonarQube>
-      <Resource
-        name='issues'
-        list={
-          <DefaultLazy>
-            <LazyIssueList />
-          </DefaultLazy>
-        }
-      />
-      <Resource
-        name='projects'
-        list={
-          <DefaultLazy>
-            <LazyProjectList />
-          </DefaultLazy>
-        }
-      />
-    </AdminSonarQube>
+    <BrowserRouter>
+      <AdminSonarQube>
+        <Resource
+          name='issues'
+          list={
+            <DefaultLazy>
+              <LazyIssueList />
+            </DefaultLazy>
+          }
+        />
+        <Resource
+          name='projects'
+          list={
+            <DefaultLazy>
+              <LazyProjectList />
+            </DefaultLazy>
+          }
+        />
+      </AdminSonarQube>
+    </BrowserRouter>
   )
 }
 
