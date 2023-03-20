dc5a96ed9fdceb4b8a88e747c17dda904cc4039c
shadowrunner11 <pieroramirez810@gmail.com>
2023-03-20 04:40:56 -0500

    :sparkles: added apollo client and codegen

--- src/__generated__/gql.ts
+++ src/__generated__/gql.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
-import * as types from './graphql'
-import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core'
+import * as types from './graphql';
+import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
 
 /**
  * Map of all GraphQL operations in the project.
@@ -13,11 +13,9 @@ import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/
  * Therefore it is highly recommended to use the babel or swc plugin for production.
  */
 const documents = {
-  '\n  query GetPaginatedAuthors($page: Int!,$limit: Float!) {\n    paginatedAuthors(page: $page, limit: $limit) {\n      pagination {\n        total\n      }\n      data {\n        _id\n        email\n      }\n    }\n  }\n':
-    types.GetPaginatedAuthorsDocument,
-  '\n  query GetPaginatedIssues($page: Int!, $filter: IssuesFilter, $limit: Int) {\n    paginatedIssues(page: $page, filter: $filter, limit: $limit) {\n      pagination {\n        total\n      }\n      data {\n        _id\n        sonarKey\n        project\n        status\n        developerEmail\n        file\n        issueCreatedAt\n        issueUpdatedAt\n        language\n        observation\n        project\n        rule\n        scope\n        severity\n        issueCreatedAt\n        startLine\n      }\n    }\n  }\n':
-    types.GetPaginatedIssuesDocument,
-}
+    "\n  query GetPaginatedAuthors($page: Int!, $limit: Float!) {\n    paginatedAuthors(page: $page, limit: $limit) {\n      pagination {\n        total\n      }\n      data {\n        _id\n        email\n      }\n    }\n  }\n": types.GetPaginatedAuthorsDocument,
+    "\n  query GetPaginatedIssues($page: Int!, $filter: IssuesFilter, $limit: Int) {\n    paginatedIssues(page: $page, filter: $filter, limit: $limit) {\n      pagination {\n        total\n      }\n      data {\n        _id\n        sonarKey\n        project\n        status\n        developerEmail\n        file\n        issueCreatedAt\n        issueUpdatedAt\n        language\n        observation\n        project\n        rule\n        scope\n        severity\n        issueCreatedAt\n        startLine\n      }\n    }\n  }\n": types.GetPaginatedIssuesDocument,
+};
 
 /**
  * The gql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
@@ -31,28 +29,19 @@ const documents = {
  * The query argument is unknown!
  * Please regenerate the types.
  */
-export function gql(source: string): unknown
+export function gql(source: string): unknown;
 
 /**
  * The gql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
-export function gql(
-  source: '\n  query GetPaginatedAuthors($page: Int!,$limit: Float!) {\n    paginatedAuthors(page: $page, limit: $limit) {\n      pagination {\n        total\n      }\n      data {\n        _id\n        email\n      }\n    }\n  }\n'
-): (typeof documents)['\n  query GetPaginatedAuthors($page: Int!,$limit: Float!) {\n    paginatedAuthors(page: $page, limit: $limit) {\n      pagination {\n        total\n      }\n      data {\n        _id\n        email\n      }\n    }\n  }\n']
+export function gql(source: "\n  query GetPaginatedAuthors($page: Int!, $limit: Float!) {\n    paginatedAuthors(page: $page, limit: $limit) {\n      pagination {\n        total\n      }\n      data {\n        _id\n        email\n      }\n    }\n  }\n"): (typeof documents)["\n  query GetPaginatedAuthors($page: Int!, $limit: Float!) {\n    paginatedAuthors(page: $page, limit: $limit) {\n      pagination {\n        total\n      }\n      data {\n        _id\n        email\n      }\n    }\n  }\n"];
 /**
  * The gql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
  */
-export function gql(
-  source: '\n  query GetPaginatedIssues($page: Int!, $filter: IssuesFilter, $limit: Int) {\n    paginatedIssues(page: $page, filter: $filter, limit: $limit) {\n      pagination {\n        total\n      }\n      data {\n        _id\n        sonarKey\n        project\n        status\n        developerEmail\n        file\n        issueCreatedAt\n        issueUpdatedAt\n        language\n        observation\n        project\n        rule\n        scope\n        severity\n        issueCreatedAt\n        startLine\n      }\n    }\n  }\n'
-): (typeof documents)['\n  query GetPaginatedIssues($page: Int!, $filter: IssuesFilter, $limit: Int) {\n    paginatedIssues(page: $page, filter: $filter, limit: $limit) {\n      pagination {\n        total\n      }\n      data {\n        _id\n        sonarKey\n        project\n        status\n        developerEmail\n        file\n        issueCreatedAt\n        issueUpdatedAt\n        language\n        observation\n        project\n        rule\n        scope\n        severity\n        issueCreatedAt\n        startLine\n      }\n    }\n  }\n']
+export function gql(source: "\n  query GetPaginatedIssues($page: Int!, $filter: IssuesFilter, $limit: Int) {\n    paginatedIssues(page: $page, filter: $filter, limit: $limit) {\n      pagination {\n        total\n      }\n      data {\n        _id\n        sonarKey\n        project\n        status\n        developerEmail\n        file\n        issueCreatedAt\n        issueUpdatedAt\n        language\n        observation\n        project\n        rule\n        scope\n        severity\n        issueCreatedAt\n        startLine\n      }\n    }\n  }\n"): (typeof documents)["\n  query GetPaginatedIssues($page: Int!, $filter: IssuesFilter, $limit: Int) {\n    paginatedIssues(page: $page, filter: $filter, limit: $limit) {\n      pagination {\n        total\n      }\n      data {\n        _id\n        sonarKey\n        project\n        status\n        developerEmail\n        file\n        issueCreatedAt\n        issueUpdatedAt\n        language\n        observation\n        project\n        rule\n        scope\n        severity\n        issueCreatedAt\n        startLine\n      }\n    }\n  }\n"];
 
 export function gql(source: string) {
-  return (documents as any)[source] ?? {}
+  return (documents as any)[source] ?? {};
 }
 
-export type DocumentType<TDocumentNode extends DocumentNode<any, any>> = TDocumentNode extends DocumentNode<
-  infer TType,
-  any
->
-  ? TType
-  : never
+export type DocumentType<TDocumentNode extends DocumentNode<any, any>> = TDocumentNode extends DocumentNode<  infer TType,  any>  ? TType  : never;
